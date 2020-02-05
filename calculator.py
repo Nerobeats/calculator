82646691c8410ed1872eef7d8806bf4b38f8a120
NeroBeats <Hani.Hamshari@Hotmail.com>
2020-02-05 15:11:02 +0200

    adding calculator.py

--- calculator.py
+++ calculator.py
@@ -1,10 +1,24 @@
-
 def main():
-	#write your code here
-	pass
-	
+	firstNumber = input("Enter the first number:  ")
+	secondNumber = input("Enter the second number: ")
+	operation = input("Choose the operation (+, -, /, *):")
+	try:
+		firstNumber = int(firstNumber)
+		secondNumber = int(secondNumber)
+	except ValueError as err:
+		print("The numbers you entered were invalid, please try again later")
 
-
+	else:
+		if operation == "*":
+			print(firstNumber * secondNumber)
+		elif operation == "+":
+			print(firstNumber + secondNumber)
+		elif operation == "-":
+				print(firstNumber - secondNumber)
+		elif operation == "/":
+			print(firstNumber / secondNumber)
+		else :
+			print("operation is not valid")
 
 if __name__ == '__main__':
 	main()
